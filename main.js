3391a633afc3b21e57266795d3b617d6b584fc70
Rafał Kotarski <you@example.com>
2023-02-28 20:07:52 +0100

    add normalize css, basic styles and javascript variables

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const rootElement = document.querySelector('#root')
+	const sections = document.querySelectorAll('section')
+	document.addEventListener('mousewheel', function (e) {
+		console.log(e.wheelDelta)
+	})
+})
